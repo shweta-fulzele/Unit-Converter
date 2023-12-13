837a97f72813db4ff08f95832ee53a2d2be62239
Shweta-Fulzele <shweta.fulzele@thinkitive.com>
2023-12-14 01:04:43 +0530

    Unit Converter Application - UI designed

---
+++ app/src/main/java/com/base/unitconverter/userinterfaceutils/extensions/TextStyleExtensions.kt
@@ -0,0 +1,42 @@
+package com.base.unitconverter.userinterfaceutils.extensions
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.base.unitconverter.ui.theme.LightGrey
+import com.base.unitconverter.ui.theme.PrimaryColor
+
+    @Composable
+    fun TextStyle.placeholder(): TextStyle {
+        return copy(
+            fontWeight = FontWeight.Light,
+            color = LightGrey
+        )
+    }
+
+    @Composable
+    fun TextStyle.h1(color: Color): TextStyle {
+        return copy(
+            fontWeight = FontWeight.Bold,
+            color = color,
+            fontSize = 24.sp
+        )
+    }
+
+
+@Composable
+fun TextStyle.h2(color: Color): TextStyle {
+    return copy(
+        fontWeight = FontWeight.SemiBold,
+        color = color,
+        fontSize = 20.sp
+    )
+}
+
+@Composable
+fun String.addColon(): String {
+    return "$this :"
+}
